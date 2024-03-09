5ad3f0d2f0a116951c2f645c2c3a42c543d0d5b6
Saqib <mohdsaqib148183@gmail.com>
2024-03-09 19:43:10 +0530

    resolve the error

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -36,8 +36,9 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  noStore()
   try {
-    noStore()
+    
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -57,8 +58,9 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  noStore()
   try {
-    noStore()
+    
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
@@ -100,6 +102,7 @@ export async function fetchFilteredInvoices(
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
   noStore()
   try {
+    
     const invoices = await sql<InvoicesTable>`
       SELECT
         invoices.id,
@@ -131,6 +134,7 @@ export async function fetchFilteredInvoices(
 export async function fetchInvoicesPages(query: string) {
   noStore()
   try {
+    
     const count = await sql`SELECT COUNT(*)
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
@@ -153,6 +157,7 @@ export async function fetchInvoicesPages(query: string) {
 export async function fetchInvoiceById(id: string) {
   noStore()
   try {
+    
     const data = await sql<InvoiceForm>`
       SELECT
         invoices.id,
@@ -196,8 +201,9 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  noStore()
+  noStore();
   try {
+    
     const data = await sql<CustomersTableType>`
 		SELECT
 		  customers.id,
@@ -230,8 +236,9 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
-  noStore()
+noStore()
   try {
+    
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0] as User;
   } catch (error) {
